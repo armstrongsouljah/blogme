e3ddd4df8be0315b53df6cdbd3b1088ac4fda165
armstrong <armstrongsouljah@gmail.com>
2020-09-21 23:19:12 +0300

    feat(blog): Implement adding a blog Item - Implement adding a blog item - Check for item existence before adding to the DOM - Create dymamic slugs

---
+++ static/js/addBlog.js
@@ -0,0 +1,63 @@
+const blogAddForm = $elem('#blogaddform');
+const blogSaveBtn = $elem('.save-btn');
+const blogCancelBtn = $elem('.cancel-btn');
+const elem = $elem('#new-item'); 
+const title = $elem('#title');
+const content = $elem('#blog-content');
+const add_modal = M.Modal.getInstance(elem);
+const blogItems = JSON.parse(localStorage.getItem('blogItems'));
+const itemsContainer = $elem(".blog-container");
+
+const generate_slug = (title) => {
+    let split_title = title.split(" ")
+    if(split_title.length>1) return title.replace(/\s/g, '-');
+    return `${title}-${Math.floor((Math.random() * 140) + 1)}`
+}
+
+
+blogAddForm.addEventListener('submit', e => {
+    e.preventDefault();
+    let blogItem;
+    if(title.value && content.value) {
+        blogItem = {
+            title: title.value,
+            content: content.value,
+            slug: generate_slug(title.value)
+        }
+    }
+
+    // add the created item into the store
+    blogItems.push(blogItem)
+    localStorage.setItem('blogItems', JSON.stringify(blogItems))
+
+    // create a new item and add it to the dom
+    blogItems.forEach(item => {
+        const card = $newElem('div')
+        const p = $newElem('p');
+        const h = $newElem('h4');
+
+        // style the item
+        card.classList.add("col", "s12", "m4", "l6", "card");
+        p.className = "flow-text";
+        h.className = "center";
+        card.setAttribute('id', item.slug)
+
+        // add content
+        h.innerText = item.title;
+        p.innerText = item.content;
+
+        card.appendChild(h)
+        card.appendChild(p)
+
+        // add item to DOM
+        let added_item = $elem(`#${item.slug}`)
+        if(!added_item) itemsContainer.appendChild(card)
+    });
+    
+    // clear the form and close the modal
+    setTimeout(blogAddForm.reset(), 2000)
+})
+
+blogAddForm.addEventListener('reset', e => {
+    add_modal.close();
+})
--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,14 @@
+const $elem = (selector) => document.querySelector(selector)
+const $elems = (selector) => document.querySelectorAll(selector)
+const $newElem = (elem) => document.createElement(elem);
+
+// initialize blog data
+localStorage.setItem('blogItems', JSON.stringify([]))
+
 // initialize tabs
-const el = document.querySelectorAll('.tabs');
+const el = $elems('.tabs');
 var instance = M.Tabs.init(el);
 
 // initialize the modals
-var elems = document.querySelectorAll('.modal');
-M.Modal.init(elems);
+var elems = $elems('.modal');
+const addFormModal = M.Modal.init(elems);
